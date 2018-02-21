254ccba182bdd5e4941b7e360c66ed013ad15566
robbaronbu <robbaron@bu.edu>
2018-02-21 10:43:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name             = 'WebConclave',
     version          = '0.0.0.1',
-    install_requires = ['pystache', 'Flask','gunicorn'],
+    install_requires = ['pystache', 'flask','gunicorn'],
     license          = 'MIT',
     url              = 'https://github.com/multiparty/WebConclave',
     description      = 'Web API for OpenShift deployment of Conclave.'
